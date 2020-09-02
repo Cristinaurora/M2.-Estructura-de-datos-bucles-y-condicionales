f98b8e4f3651c2ed5a151c9148d6ca837f6f37fa
Cristinaurora <cristina.leonsarabia@gmail.com>
2020-09-02 10:39:27 +0200

    Fase 1
    Tablas y blucles

---
+++ src/Fase1.java
@@ -0,0 +1,14 @@
+
+public class Fase1 {
+
+	public static void main(String[] args) {
+
+		char[] nombre = {'a', 'u', 'r', 'o', 'r', 'a'};
+		
+		for (char c:nombre) {
+			System.out.println(c);
+		}
+
+	}
+
+}
