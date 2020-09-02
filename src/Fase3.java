ee055d37d611db1e39e25805a1b47b492cc64cc3
Cristinaurora <cristina.leonsarabia@gmail.com>
2020-09-02 10:44:43 +0200

    Fase 3

---
+++ src/Fase3.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class Fase3 {
+
+	public static void main(String[] args) {
+
+        Map<Character, Integer> nombre = new HashMap<>();
+        
+        nombre.put('a', 2);
+        nombre.put('u', 1);
+        nombre.put('r', 2);
+        nombre.put('o', 1);
+
+        //TODO bucle?
+	}
+
+}
